c16020c4dcce01f37d06b07fd719ec0db0b39e52
Shubham Bharti <107488607+shubhambharti4716@users.noreply.github.com>
2023-09-11 12:42:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
-//your code here
+// Given array of band names
+let bandNames = ['The Rolling Stone', 'Led Zeppelin', 'Aerosmith', 'The Beatles', 'Nirvana'];
 
+// Function to remove articles and return the modified band name
+function removeArticles(name) {
+  return name.replace(/^(a |an |the )/i, '').trim();
+}
+
+// Remove articles from all band names
+let modifiedBandNames = bandNames.map(removeArticles);
+
+// Sort the modified band names lexicographically
+modifiedBandNames.sort();
+
+// Create an ul element with id 'band'
+let ulElement = document.createElement('ul');
+ulElement.id = 'band';
+
+// Add li elements to the ul element
+modifiedBandNames.forEach(function (bandName) {
+  let liElement = document.createElement('li');
+  liElement.textContent = bandName;
+  ulElement.appendChild(liElement);
+});
+
+// Append the ul element to the document body or any desired location
+document.body.appendChild(ulElement);
